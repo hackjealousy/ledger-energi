a3e466097fe05d37e9b0b51396ef86d76f34d9fd
Joshua Lackey <jl@thre.at>
2019-10-16 20:27:15 -0400

    add lock indicator to get_total_balance

--- lwallet/scripts/get_total_balance.py
+++ lwallet/scripts/get_total_balance.py
@@ -11,8 +11,9 @@ for k in addr_d:
     ae = addr_d[k]
     path = 'm/44\'/9797\'/%(account)d\'/%(change)d/%(index)d' % ae
 
-    for u in addr_d[k].get('utxos', []):
+    for u in ae.get('utxos', []):
         tot += u['satoshis']
-        print('%s: %s: %f' % (path, u['address'], float(u['satoshis']) / 10**8))
+
+        print('%s: %s: %f %s' % (path, u['address'], float(u['satoshis']) / 10**8, '(LOCKED)' if walletdb.is_locked_txid(u['txid'], u['nout']) else ''))
 
 print('Total: %f' % (float(tot) / 10**8))
